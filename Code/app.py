2055c2b27b3141e7a3bbde0e3daaf61a6e7c6bd4
Nimish Kumar <nimishkumar0305@gmail.com>
2024-06-28 01:04:33 +0530

    Add db + Admin Login

--- Code/app.py
+++ Code/app.py
@@ -1,7 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import hashlib
+from flask_sqlalchemy import SQLAlchemy
+
+# * ---------------- MODELS ----------------#
+
+
+
 
 app = Flask(__name__)
 
+app.secret_key = 'promopulse-123-$%^-mad2-proj*'  # Change this to a strong secret key
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///promopulse.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# ! ---------------- MODELS ----------------#
+
+# * --- Admin Table --- #
+class Admin(db.Model):
+    __tablename__ = 'admin'
+    AdminID = db.Column(db.Integer, primary_key=True)
+    Username = db.Column(db.String(50), nullable=False)
+    Password = db.Column(db.String(255), nullable=False)
+    Email = db.Column(db.String(100), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f"<{self.Username} | {self.Password} | {self.Email}>"
+
+
+# ! ---------------- ROUTES ----------------#
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -13,5 +42,29 @@ def login():
 @app.route('/signup')
 def signup():
     return render_template('signup.html')
+
+@app.route('/admin')
+def admin_login_page():
+    return render_template('admin-login.html')
+
+@app.route('/admin/login', methods=['GET', 'POST'])
+def admin_login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        print(email,password)
+        admin = Admin.query.filter_by(Email=email).first()
+        if admin and password == admin.Password:
+            return redirect(url_for('admin_dashboard'))
+        else:
+            return "<h1>Invalid uname or pwd</h1>"
+
+
+@app.route('/admin/dashboard')
+def admin_dashboard():
+    return render_template('admin-dash.html')
+
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True,port=5500)
