2322a25ddb3654708f139921e75b8338bcd364dd
Nimish Kumar <nimishkumar0305@gmail.com>
2024-07-11 00:23:35 +0530

    Admin Login Styling

--- Code/app.py
+++ Code/app.py
@@ -1,16 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 import hashlib
 from flask_sqlalchemy import SQLAlchemy
-
-# * ---------------- MODELS ----------------#
-
-
-
 
 app = Flask(__name__)
 
 app.secret_key = 'promopulse-123-$%^-mad2-proj*'  # Change this to a strong secret key
-
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///promopulse.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -55,14 +49,22 @@ def admin_login():
         print(email,password)
         admin = Admin.query.filter_by(Email=email).first()
         if admin and password == admin.Password:
+            session['admin'] = True
             return redirect(url_for('admin_dashboard'))
         else:
             return "<h1>Invalid uname or pwd</h1>"
 
 
+def isAdminLogin():
+    return 'admin' in session and session['admin'] == True
+
+
 @app.route('/admin/dashboard')
 def admin_dashboard():
-    return render_template('admin-dash.html')
+    if(isAdminLogin()):
+        return render_template('admin-dash.html')
+    else:
+        return "<h1>Must be logged in as Admin</h1>"
 
 if __name__ == '__main__':
     with app.app_context():
